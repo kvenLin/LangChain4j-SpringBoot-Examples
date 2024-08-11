d9617879919570329af1b0b429b8d28bad2279c6
kevin <clf1256233771@gmail.com>
2024-08-11 19:12:14 +0800

    init DatabaseRetriever-example

---
+++ DatabaseRetriever-example/src/main/java/com/example/databaseretrieverexample/DatabaseRetrieverExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.databaseretrieverexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseRetrieverExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseRetrieverExampleApplication.class, args);
+	}
+
+}
