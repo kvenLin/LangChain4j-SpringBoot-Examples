f205b329c018e380b5401be4d3d0e04fadcb54fc
kevin <clf1256233771@gmail.com>
2024-08-11 19:12:14 +0800

    init DatabaseRetriever-example

---
+++ DatabaseRetriever-example/src/test/java/com/example/databaseretrieverexample/DatabaseRetrieverExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databaseretrieverexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseRetrieverExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
